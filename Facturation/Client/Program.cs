deeba9c1606050daef2a74c82746296fbe560143
Madezz-o <55086670+Madezz-o@users.noreply.github.com>
2020-12-07 17:33:25 +0100

    V2

--- Facturation/Client/Program.cs
+++ Facturation/Client/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Facturation.Shared;
 
 namespace Facturation.Client
 {
@@ -17,6 +18,19 @@ namespace Facturation.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            Facture facture1 = new Facture("LESTON", "00001", new DateTime(2020,12,30), new DateTime(2020, 12, 30), 104850000, 45632);
+            Facture facture2 = new Facture("MADEZO", "00002", new DateTime(2020, 12, 30), new DateTime(2020, 12, 30), 154650, 42132);
+            Facture facture3 = new Facture("BAZATS", "00003", new DateTime(2020, 12, 30), new DateTime(2020, 12, 30), 93, 6562);
+            Facture facture4 = new Facture("BENETIER", "69420", new DateTime(2020, 12, 30), new DateTime(2020, 12, 30), 69420, 2789);
+
+            BusinessData Factures = new BusinessData();
+
+            Factures.addFacture(facture1);
+            Factures.addFacture(facture2);
+            Factures.addFacture(facture3);
+            Factures.addFacture(facture4);
+
+            builder.Services.AddSingleton<IBusinessData>(sp => new BusinessData());
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
